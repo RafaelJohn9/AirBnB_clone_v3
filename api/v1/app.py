ef655f6be599352ed07e591d5ddbfa25599c1c3f
RafaelJohn9 <rafaeljohb@gmail.com>
2023-10-28 14:45:48 +0300

    corrected app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,7 +5,7 @@
 from api.v1.views import app_views
 from flask import Flask, make_response
 from models import storage
-from os import environ
+from os import getenv
 
 app = Flask(__name__)
 
@@ -24,7 +24,6 @@ def not_found(error):
 
 if __name__ == "__main__":
     """ get host and port from env var """
-    host = environ.get("HBNB_API_HOST", "0.0.0.0")
-    port = int(environ.get("HBNB_API_PORT", 5000))
-
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(getenv("HBNB_API_PORT", 5000))
     app.run(host=host, port=port, threaded=True)
